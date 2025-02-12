82fa2fe88f76eaaa1f8794ea0594201e9427ecf5
Pokar Petr <pokar.petr@gmail.com>
2025-02-12 16:47:24 +0100

    modified SeatController

--- src/app/Models/Seat.php
+++ src/app/Models/Seat.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\Aircraft;
 use Illuminate\Database\Eloquent\Model;
 
 class Seat extends Model
 {
-    //
+    
+    public function aircraft()
+    {
+        return $this->belongsTo(Aircraft::class, 'aircraft_code', 'aircraft_code');
+    }
 }
